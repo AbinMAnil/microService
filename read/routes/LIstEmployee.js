7a65a86e5550bd2229c5f80552caf64a5d51ece5
AbinMAnil <abinmanil80@gmail.com>
2023-04-26 17:26:57 +0530

    command to create table

--- read/routes/LIstEmployee.js
+++ read/routes/LIstEmployee.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const { listAllEmployees , getEmployDetails   } = require("../controller/listEmployee");
+const pool = require("../db/postgresConnection");
 const router = express.Router();
 
+router.get('/addTable' , (erq, res) => { 
+
+    pool.query( `create table employees (
+        id SERIAL PRIMARY KEY,
+        firstName VARCHAR(250),
+        lastName VARCHAR(250),
+        email VARCHAR(250),
+        dept VARCHAR(250),
+        salary INT,
+        birthDate DATE,
+        joinDate DATE,
+        title VARCHAR(250)
+    )` , (err , result) => {
+        if(err) res.status(500).json({message: "something went wrong"});
+        else res.status(200).json({message: "Table created success", ...result})
+    } )
+})
 router.get("/employees" , listAllEmployees);
 router.get("/employees/:id" , getEmployDetails);
 
